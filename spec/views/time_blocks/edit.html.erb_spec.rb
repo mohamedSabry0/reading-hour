ca0c5f98d204f9e554c89189af0f5297d056f88d
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-12-07 23:16:24 +0200

    set up basic models and migrate

---
+++ spec/views/time_blocks/edit.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "time_blocks/edit", type: :view do
+  before(:each) do
+    @time_block = assign(:time_block, TimeBlock.create!(
+      name: "MyString",
+      amount: 1
+    ))
+  end
+
+  it "renders the edit time_block form" do
+    render
+
+    assert_select "form[action=?][method=?]", time_block_path(@time_block), "post" do
+
+      assert_select "input[name=?]", "time_block[name]"
+
+      assert_select "input[name=?]", "time_block[amount]"
+    end
+  end
+end
---
+++ spec/views/time_blocks/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "time_blocks/new", type: :view do
+  before(:each) do
+    assign(:time_block, TimeBlock.new(
+      name: "MyString",
+      amount: 1
+    ))
+  end
+
+  it "renders new time_block form" do
+    render
+
+    assert_select "form[action=?][method=?]", time_blocks_path, "post" do
+
+      assert_select "input[name=?]", "time_block[name]"
+
+      assert_select "input[name=?]", "time_block[amount]"
+    end
+  end
+end
