b836c8774d24fc5e1cba4078a26fe6b95c072892
mohamedSabry0 <mohamedsabry3023@gmail.com>
2021-01-07 20:58:13 +0200

    remove minitest folder and run rubocop fix

--- spec/models/grouping_spec.rb
+++ spec/models/grouping_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Grouping, type: :model do
-    describe 'associations' do
-        it { should belong_to(:time_block) }
-        it { should belong_to(:group) }
-    end
+  describe 'associations' do
+    it { should belong_to(:time_block) }
+    it { should belong_to(:group) }
+  end
 end
--- spec/models/time_block_spec.rb
+++ spec/models/time_block_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe TimeBlock, type: :model do
-    describe 'validations' do
-        it { should validate_presence_of(:name) }
-        it { should validate_length_of(:name).is_at_least(3).is_at_most(15) }
-        it { should validate_presence_of(:amount) }
-    end
-    describe 'associations' do
-        it { should belong_to(:author).class_name('User') }
-        it { should have_many(:groupings).dependent(:destroy) }
-        it { should have_many(:groups).through(:groupings)}
-    end
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(3).is_at_most(15) }
+    it { should validate_presence_of(:amount) }
+  end
+  describe 'associations' do
+    it { should belong_to(:author).class_name('User') }
+    it { should have_many(:groupings).dependent(:destroy) }
+    it { should have_many(:groups).through(:groupings) }
+  end
 end
