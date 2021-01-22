0c0bdc3c79bd37d3c196968e5c93879e425a1a1c
mohamedSabry0 <mohamedsabry3023@gmail.com>
2021-01-22 18:41:26 +0200

    add feature tests for users

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.feature 'Users', type: :feature do
+    describe 'new user' do
+        it 'sign up successfully with valid data' do
+            visit 'users/sign_up'
+            fill_in 'Username', with: 'per'
+            fill_in 'Email', with: 'per@e.com'
+            fill_in 'Password', with: '123123'
+            fill_in 'Password confirmation', with: '123123'
+            click_on 'Sign up'
+            expect(page).to have_content('Welcome! You have signed up successfully.')
+        end
+        it 'error appears with required data' do
+            visit 'users/sign_up'
+            fill_in 'Username', with: nil
+            fill_in 'Email', with: nil
+            fill_in 'Password', with: nil
+            fill_in 'Password confirmation', with: nil
+            click_on 'Sign up'
+
+            expect(page).to have_content("Email can't be blank")
+            expect(page).to have_content("Password can't be blank")
+            expect(page).to have_content("Username can't be blank")
+            expect(page).to have_content("Username is too short (minimum is 3 characters)")
+        end
+    end
+    before do
+        visit 'users/sign_up'
+        fill_in 'Username', with: 'per2'
+        fill_in 'Email', with: 'per2@e.com'
+        fill_in 'Password', with: '123123'
+        fill_in 'Password confirmation', with: '123123'
+        click_on 'Sign up'
+        click_on 'Log Out'
+    end
+    describe 'login' do
+        it "can't login with invalid data" do
+            visit 'users/sign_in'
+            fill_in 'Login', with: nil
+            fill_in 'Password', with: nil
+            click_on 'Log in'
+            expect(page).to have_content('Invalid Login or password')
+        end
+        it "can login with username" do
+            visit 'users/sign_in'
+            fill_in 'Login', with: 'per2'
+            fill_in 'Password', with: '123123'
+            click_on 'Log in'
+            expect(page).to have_content('Signed in successfully')
+        end
+        it "can login with email" do
+            visit 'users/sign_in'
+            fill_in 'Login', with: 'per2@e.com'
+            fill_in 'Password', with: '123123'
+            click_on 'Log in'
+            expect(page).to have_content('Signed in successfully')
+        end
+    end
+end
