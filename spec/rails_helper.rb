3a484cd3ece4fa2a8a33db945ac07a73ec7bb40d
mohamedSabry0 <mohamedsabry3023@gmail.com>
2021-01-29 22:43:33 +0200

    fix linters

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,7 +41,7 @@ RSpec.configure do |config|
   end
 end
 
-Capybara.default_driver = (ENV['GUI']=='true' ? :selenium : :selenium_headless)
+Capybara.default_driver = (ENV['GUI'] == 'true' ? :selenium : :selenium_headless)
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
